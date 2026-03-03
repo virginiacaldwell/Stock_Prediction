0c7ada57dcc13956f1fcd008437bb711fcc4df6c
virginiacaldwell <virginiacaldwell@icloud.com>
2026-03-03 13:35:44 -0600

    Update pipeline reference in MODEL_INFO

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -58,7 +58,7 @@ df_features = extract_features_pair()
 MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
-        "pipeline": 'sklearn-pipeline-deployment',
+        "pipeline": 'finalized_pair_model.tar.gz',
         "keys": ["MPWR", "AAPL"],
         "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["MPWR", "AAPL"]]
 }
