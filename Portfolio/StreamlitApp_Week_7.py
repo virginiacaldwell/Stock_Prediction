fa95acdc2b3816882e4643ac0bc9d338f1337fe0
virginiacaldwell <virginiacaldwell@icloud.com>
2026-03-03 13:46:04 -0600

    Fix typo in top_feature calculation method

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -124,7 +124,7 @@ def display_explanation(input_df, session, aws_bucket):
     shap.plots.waterfall(shap_values[0, :, 0])
     st.pyplot(fig)
     # top feature   
-    top_feature = pd.Series(shap_values[0, :, 0].values, index=shap_values[0, :, 0].feature_names).abs().idmax()
+    top_feature = pd.Series(shap_values[0, :, 0].values, index=shap_values[0, :, 0].feature_names).abs().idxmax()
     st.info(f"**Business Insight:** The most influential factor in this decision was **{top_feature}**.")
 
 # Streamlit UI
