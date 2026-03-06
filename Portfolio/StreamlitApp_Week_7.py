f249ee82ef378496900f4d2f605ce35ec0931355
virginiacaldwell <virginiacaldwell@icloud.com>
2026-03-05 23:08:25 -0600

    Update model keys and inputs in Streamlit app

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -59,8 +59,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["MPWR", "AAPL"],
-        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["MPWR", "AAPL"]]
+        "keys": ["AMD", "DHI"],
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["AMD", "DHI"]]
 }
 
 def load_pipeline(_session, bucket, key):
