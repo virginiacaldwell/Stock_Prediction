b477b083f5bef935eaab653a89a8d847c4660517
virginiacaldwell <virginiacaldwell@icloud.com>
2026-04-27 23:29:38 -0500

    Refactor Streamlit app and update model configurations
    Refactor and clean up the Streamlit app code, including removing unused imports, updating model information, and modifying data loading logic.

--- Portfolio/StreamlitApp_Project.py
+++ Portfolio/StreamlitApp_Project.py
@@ -4,57 +4,52 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import posixpath
-
+ 
 import joblib
 import tarfile
 import tempfile
-
+ 
 import boto3
 import sagemaker
 from sagemaker.predictor import Predictor
 from sagemaker.serializers import CSVSerializer
-from sagemaker.serializers import JSONSerializer 
-from sagemaker.deserializers import JSONDeserializer 
+from sagemaker.serializers import JSONSerializer
+from sagemaker.deserializers import JSONDeserializer
 from sagemaker.serializers import NumpySerializer
 from sagemaker.deserializers import NumpyDeserializer
-
-
+ 
 from sklearn.pipeline import Pipeline
 import shap
-
+ 
 from joblib import dump
 from joblib import load
 
 
-
 # Setup & Path Configuration
 warnings.simplefilter("ignore")
-
+ 
 # Fix path for Streamlit Cloud (ensure 'src' is findable)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
 if project_root not in sys.path:
     sys.path.append(project_root)
-
+ 
 #from src.feature_utils import extract_features
-from src.Custom_Classes import FeatureEngineer, AutoPowerTransformer
-
+#from src.Custom_Classes import DropHighMissingCols, TransactionFeatureEngineer, DropHighCorrelation
+ 
 file_path = os.path.join(project_root, 'Portfolio/X_train.csv')
-
-try:
-    dataset = pd.read_csv(file_path)
-except pd.errors.EmptyDataError:
-    st.error("The data file is empty. Please check your data source.")
-    dataset = pd.DataFrame() # Create an empty dataframe so the app doesn't break
-dataset = dataset.loc[:, ~dataset.columns.str.contains('^Unnamed')]
-
+ 
+dataset = pd.read_csv(file_path)
+dataset = dataset.drop(['Unnamed: 0'],axis=1)
+#dataset = dataset.loc[:, ~dataset.columns.str.contains('^Unnamed')]
+ 
 # Access the secrets
 aws_id = st.secrets["aws_credentials"]["AWS_ACCESS_KEY_ID"]
 aws_secret = st.secrets["aws_credentials"]["AWS_SECRET_ACCESS_KEY"]
 aws_token = st.secrets["aws_credentials"]["AWS_SESSION_TOKEN"]
 aws_bucket = st.secrets["aws_credentials"]["AWS_BUCKET"]
 aws_endpoint = st.secrets["aws_credentials"]["AWS_ENDPOINT"]
-
+ 
 # AWS Session Management
 @st.cache_resource # Use this to avoid downloading the file every time the page refreshes
 def get_session(aws_id, aws_secret, aws_token):
@@ -64,61 +59,60 @@ def get_session(aws_id, aws_secret, aws_token):
         aws_session_token=aws_token,
         region_name='us-east-1'
     )
-
+ 
 session = get_session(aws_id, aws_secret, aws_token)
 sm_session = sagemaker.Session(boto_session=session)
-
+ 
 # Data & Model Configuration
-
+ 
 MODEL_INFO = {
     "endpoint"  : aws_endpoint,
-    "explainer" : "explainer_fraud.shap",
-    "pipeline"  : "fine_tuned_pipeline.tar.gz",
-    "keys"      : ['TRANSACTIONAMT','CARD1','CARD2'],
-    "inputs"    : [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['TRANSACTIONAMT','CARD1','CARD2']]
+    "explainer" : "explainer_sentiment.shap",
+    "pipeline"  : "finalized_fraud_model.tar.gz",
+    "keys"      : ['TransactionAmt','card6_freq_enc','card3','C12'],
+    "inputs"    : [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['TransactionAmt','card6_freq_enc','card3','C12']]
 }
-
-
+ 
 def load_pipeline(_session, bucket, key):
     s3_client = _session.client('s3')
     filename=MODEL_INFO["pipeline"]
-
+ 
     s3_client.download_file(
-        Filename=filename, 
-        Bucket=bucket, 
+        Filename=filename,
+        Bucket=bucket,
         Key= f"{key}/{os.path.basename(filename)}")
         # Extract the .joblib file from the .tar.gz
     with tarfile.open(filename, "r:gz") as tar:
         tar.extractall(path=".")
-        #joblib_file = [f for f in tar.getnames() if f.endswith('.joblib')][0]
-        joblib_file = [f for f in tar.getnames() if f.endswith('.pkl')][0]
-    
-
+        joblib_file = [f for f in tar.getnames() if f.endswith('.joblib')][0]
+        #joblib_file = [f for f in tar.getnames() if f.endswith('.pkl')][0]
+   
+ 
     # Load the full pipeline
     return joblib.load(f"{joblib_file}")
-
+ 
 def load_shap_explainer(_session, bucket, key, local_path):
     s3_client = _session.client('s3')
     local_path = local_path
-
+ 
     # Only download if it doesn't exist locally to save time
     if not os.path.exists(local_path):
         s3_client.download_file(Filename=local_path, Bucket=bucket, Key=key)
-        
+       
     with open(local_path, "rb") as f:
         return load(f)
         #return shap.Explainer.load(f)
-
+ 
 # Prediction Logic
 def call_model_api(input_df):
-
+ 
     predictor = Predictor(
         endpoint_name=MODEL_INFO["endpoint"],
         sagemaker_session=sm_session,
-        serializer=JSONSerializer(), 
-        deserializer=NumpyDeserializer() 
+        serializer=JSONSerializer(),
+        deserializer=NumpyDeserializer()
     )
-
+ 
     try:
         raw_pred = predictor.predict(input_df)
         pred_val = pd.DataFrame(raw_pred).values[-1][0]
@@ -127,72 +121,49 @@ def call_model_api(input_df):
         return mapping.get(pred_val), 200
     except Exception as e:
         return f"Error: {str(e)}", 500
-
+ 
 # Local Explainability
 def display_explanation(input_df, session, aws_bucket):
     explainer_name = MODEL_INFO["explainer"]
     explainer = load_shap_explainer(session, aws_bucket, posixpath.join('explainer', explainer_name),os.path.join(tempfile.gettempdir(), explainer_name))
-    
+   
     best_pipeline = load_pipeline(session, aws_bucket, 'sklearn-pipeline-deployment')
-    preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[:-3])
+    preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[:-2])
     input_df=pd.DataFrame(input_df)
     input_df_transformed = preprocessing_pipeline.transform(input_df)
     feature_names = best_pipeline[:-2].get_feature_names_out()
     input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
     shap_values = explainer(input_df_transformed)
-    
+   
     st.subheader("🔍 Decision Transparency (SHAP)")
     fig, ax = plt.subplots(figsize=(10, 4))
     shap.plots.waterfall(shap_values[0, :, 1])  # class 1 = fraud
     st.pyplot(fig)
     top_feature = pd.Series(shap_values[0, :, 1].values, index=shap_values[0, :, 1].feature_names).abs().idxmax()
     st.info(f"**Business Insight:** The most influential factor in this decision was **{top_feature}**.")
-
-
+ 
 # Streamlit UI
 st.set_page_config(page_title="ML Deployment", layout="wide")
 st.title("👨‍💻 ML Deployment")
-
+ 
 with st.form("pred_form"):
     st.subheader(f"Inputs")
     cols = st.columns(2)
     user_inputs = {}
-    
+   
     for i, inp in enumerate(MODEL_INFO["inputs"]):
         with cols[i % 2]:
             user_inputs[inp['name']] = st.number_input(
                 inp['name'].replace('_', ' ').upper(),
                 min_value=inp['min'], max_value=inp['max'], value=inp['default'], step=inp['step']
-            ) 
+            )
+   
     submitted = st.form_submit_button("Run Prediction")
-
-if submitted:
- # 1. Create a single row from your slider inputs
-# The keys here must match 'TRANSACTIONAMT', 'CARD1', 'CARD2' exactly
-    input_row = [user_inputs[k] for k in MODEL_INFO["keys"]]
-        
-# 2. THE FIX: Multiply that row by 20 to create a "history" 
-# so the FeatureEngineer doesn't return NaNs!
-    input_df = pd.DataFrame([input_row] * 20, columns=MODEL_INFO["keys"])
-        
-# 3. Call your prediction function
-    with st.spinner("Calling SageMaker..."):
-        prediction, status = call_model_api(input_df.values.tolist())
-            
-    if status == 200:
-        st.success(f"Result: {prediction}")
-                
-        # 4. Trigger the SHAP explanation
-        st.subheader("Why this prediction?")
-        display_explanation(input_df, session, aws_bucket)
-    else:
-        st.error(f"Endpoint Error: {prediction}")
-
-
+ 
 original = dataset.iloc[0:1].to_dict()
 original.update(user_inputs)
 if submitted:
-
+ 
     res, status = call_model_api(original)
     if status == 200:
         st.metric("Prediction Result", res)
